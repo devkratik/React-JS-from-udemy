aac65ba5c785dfd45dfa4a9c53cf94c313c54028
Kratik777 <kratikbhatt6@gmail.com>
2026-05-27 21:44:32 +0530

    Conditional Rendering useState

---
+++ Section 4/02 Vite+React=Love/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      {/* Title */}
+      <h1 className="container__title">
+        Vite + React = {count >= 3 ? "Love" : ""}
+      </h1>
+
+      {/* Logo Container */}
+      <div className="container__logo">
+        <figure className="logo">
+          <img
+            className={`logo__vite ${count >= 1 ? "active" : ""} `}
+            src="/vite.svg"
+            alt="logo-vite"
+          />
+        </figure>
+        <p className="logo__connector">+</p>
+        <figure className="logo">
+          <img
+            className={`logo__react ${count >= 2 ? "active" : ""} `}
+            src="/react.svg"
+            alt="logo-react"
+          />
+        </figure>
+        <p className="logo__connector">=</p>
+        <figure className="logo">
+          <img
+            className={`logo__love ${count >= 3 ? "active" : ""} `}
+            src="/love.svg"
+            alt="logo-love"
+          />
+        </figure>
+      </div>
+
+      <hr />
+
+      {/* Counter */}
+
+      <div className="counter">
+        <div className="counter__info">Count is {count}</div>
+        <div className="counter__controls">
+          <button className="btn" onClick={() => setCount((prev) => prev + 1)}>
+            +1
+          </button>
+          <button className="btn" onClick={() => setCount((prev) => prev - 1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => setCount(0)}>
+            Reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
